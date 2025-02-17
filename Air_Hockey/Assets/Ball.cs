22ecd916194bbc70a56599005152b443e13f7499
JesCat16 <jmdjesus83@gmail.com>
2025-02-17 15:44:34 -0300

    Add Obstáculos
    Adicionando Obstáculos

--- Air_Hockey/Assets/Ball.cs
+++ Air_Hockey/Assets/Ball.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    Vector3 direcao;
     // Start is called before the first frame update
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        direcao = Vector3.forward;
+
+    }
+    private void Update()
+    {
+        rb2d.AddForce(direcao, (ForceMode2D)ForceMode.VelocityChange);
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.collider.CompareTag("Player"))
         {
-            Vector2 vel;
-            vel.x = rb2d.velocity.x;
-            vel.y = rb2d.velocity.y;
-            rb2d.velocity = vel;
+            direcao = Vector3.Reflect(rb2d.velocity, coll.contacts[0].normal);
         }
 
-        if (coll.collider.CompareTag("bot"))
+        else if (coll.collider.CompareTag("bot"))
         {
-            Vector2 vel;
-            vel.x = rb2d.velocity.x;
-            vel.y = rb2d.velocity.y;
-            rb2d.velocity = vel;
+            direcao = Vector3.Reflect(rb2d.velocity, coll.contacts[0].normal);
         }
+
+        else
+        {
+            direcao = Vector3.Reflect(rb2d.velocity, coll.contacts[0].normal);
+        }
+       
     }
     void ResetBall()
     {
