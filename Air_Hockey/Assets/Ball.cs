da5c479f52ff11c5b08301c0bfe86a14e22bfe14
JesCat16 <jmdjesus83@gmail.com>
2025-02-18 12:43:44 -0300

    Update Ball
    Ajuste agora bola funciona, ajuste também da posição das paredes e dos gols

--- Air_Hockey/Assets/Ball.cs
+++ Air_Hockey/Assets/Ball.cs
@@ -4,41 +4,58 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
-    Vector3 direcao;
-    // Start is called before the first frame update
+    private Rigidbody2D rb2d;               // Define o corpo rigido 2D que representa a bola
 
+    // inicializa a bola randomicamente para esquerda ou direita
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, 15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+    // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        direcao = Vector3.forward;
+        rb2d = GetComponent<Rigidbody2D>(); // Inicializa o objeto bola
+        GoBall();    // Chama a função GoBall após 2 segundos
+    }
 
-    }
-    private void Update()
-    {
-        rb2d.AddForce(direcao, (ForceMode2D)ForceMode.VelocityChange);
-    }
+    // Determina o comportamento da bola nas colisões com os Players (raquetes)
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.collider.CompareTag("Player"))
         {
-            direcao = Vector3.Reflect(rb2d.velocity, coll.contacts[0].normal);
+            Vector2 vel;
+            vel.x = rb2d.velocity.x + coll.collider.attachedRigidbody.velocity.x;
+            vel.y = rb2d.velocity.y + coll.collider.attachedRigidbody.velocity.y;
+            rb2d.velocity = vel;
         }
-
-        else if (coll.collider.CompareTag("bot"))
+       else if (coll.collider.CompareTag("bot"))
         {
-            direcao = Vector3.Reflect(rb2d.velocity, coll.contacts[0].normal);
+            Vector2 vel;
+            vel.x = rb2d.velocity.x + coll.collider.attachedRigidbody.velocity.x;
+            vel.y = rb2d.velocity.y + coll.collider.attachedRigidbody.velocity.y;
+            rb2d.velocity = vel;
         }
-
-        else
-        {
-            direcao = Vector3.Reflect(rb2d.velocity, coll.contacts[0].normal);
-        }
-       
     }
+
+    // Reinicializa a posição e velocidade da bola
     void ResetBall()
     {
         rb2d.velocity = Vector2.zero;
         transform.position = Vector2.zero;
+    }
+
+    // Reinicializa o jogo
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall", 1);
     }
 }
