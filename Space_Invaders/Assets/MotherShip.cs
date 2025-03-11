e0a2a8d36bb3033c9cd569168eb1898acf340500
JesCat16 <jmdjesus83@gmail.com>
2025-03-11 14:16:22 -0300

    MotherShip Fix

--- Space_Invaders/Assets/MotherShip.cs
+++ Space_Invaders/Assets/MotherShip.cs
@@ -7,19 +7,25 @@ public class MotherShip : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private bool spawned;
-    private int cycle = 10;
     private float spawnTime;
-    private float timer = 0.0f;
-    private float waitTime = 1.0f;
+    private int rand;
     private float speed = 5.0f;
+<<<<<<< Updated upstream
     private float boundX = 7.25f;
     private Transform locateWall;
+=======
+    private float Bound = 7.0f;
+>>>>>>> Stashed changes
     public static System.Action MotherKill;
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         locateWall = GameObject.FindGameObjectWithTag("wall2").transform;
+=======
+        rand = Random.Range(0, 1);
+>>>>>>> Stashed changes
         spawnTime = Random.Range(1,10);
         rb2d = GetComponent<Rigidbody2D>();
         var vel = rb2d.velocity;
@@ -31,7 +37,28 @@ public class MotherShip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
        
+=======
+        if (!spawned) return;
+        if(rand == 0)
+        {
+            transform.position += speed * Time.deltaTime * Vector3.right;
+            if(transform.position.x >= Bound)
+            {
+                Despawn();
+            }
+        }
+        else
+        {
+            transform.position += speed * Time.deltaTime * Vector3.left;
+            if (transform.position.x >= -Bound)
+            {
+                Despawn();
+            }
+        }
+        
+>>>>>>> Stashed changes
     }
     private void Despawn()
     {
@@ -43,6 +70,14 @@ public class MotherShip : MonoBehaviour
     private void Spawn()
     {
         spawned = true;
+        if(rand == 0)
+        {
+           transform.position = new Vector2(-Bound,transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(Bound, transform.position.y);
+        }
         gameObject.SetActive(true);
         transform.position = Vector2.MoveTowards(transform.position, locateWall.position, speed * Time.deltaTime);
         var pos = transform.position;
