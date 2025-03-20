cc4a35f063d183c6c7a610e1a5c1489ba2adef7e
JesCat16 <jmdjesus83@gmail.com>
2025-03-20 10:10:15 -0300

    Fix

--- Space_Invaders/Assets/Invaders.cs
+++ Space_Invaders/Assets/Invaders.cs
@@ -8,32 +8,72 @@ public class Invaders : MonoBehaviour
     private float timer = 0.0f;
     private float waitTime = 1.0f;
     private float speed = 2.0f;
+    private int counter = 0;
+    private float AttackRate = 1.0f;
+    public Lazer_Missel lazer;
+    public static System.Action killed;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();  
-
+        rb2d = GetComponent<Rigidbody2D>();
         var vel = rb2d.velocity;
         vel.x = speed;
         rb2d.velocity = vel;
-
+        InvokeRepeating(nameof(lazerAttack), AttackRate, AttackRate);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var vel = rb2d.velocity;
         timer += Time.deltaTime;
-        if (timer >= waitTime){
+        if (timer >= waitTime)
+        {
             ChangeState();
+            counter += 1;
             timer = 0.0f;
+            if (counter > 5)
+            {
+                vel.x *= 2;
+                ChangeStateDown();
+                counter = 0;
+            }
         }
     }
 
-    void ChangeState(){
+    void ChangeState()
+    {
         var vel = rb2d.velocity;
         vel.x *= -1;
         rb2d.velocity = vel;
+    }
+
+    void ChangeStateDown()
+    {
+        Vector3 position = transform.position;
+        position.y -= 1.0f;
+        transform.position = position;
+    }
+
+    private void lazerAttack()
+    {
+        if ((Random.value < GameObject.FindGameObjectsWithTag("invader").Length) && GameObject.FindGameObjectsWithTag("lazer").Length < 5)
+        {
+            if (gameObject.activeSelf == true)
+            {
+                Instantiate(lazer, transform.position, Quaternion.identity);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("missel"))
+        {
+            speed += 1;
+            killed.Invoke();
+            gameObject.SetActive(false);
+        }
     }
 
 }
--- Space_Invaders/Assets/player.cs
+++ Space_Invaders/Assets/player.cs
@@ -5,14 +5,72 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     // Start is called before the first frame update
+    public KeyCode moveLeft = KeyCode.A;      // Move a raquete para cima
+    public KeyCode moveRight = KeyCode.D;    // Move a raquete para baixo
+    public KeyCode Shoot = KeyCode.Space;
+    public float speed = 10.0f;             // Define a velocidade da bola
+    public float boundX;            // Define os limites em X
+    public Rigidbody2D rb2d;               // Define o corpo rigido 2D que representa a raquete
+    public Lazer_Missel missel;
+    public static System.Action hit;
+    public static System.Action gameOver;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var vel = rb2d.velocity;
+        if (Input.GetKey(moveLeft))
+        {
+            vel.x = -speed;
+        }
+        else if (Input.GetKey(moveRight))
+        {
+            vel.x = speed;
+        }
+        else
+        {
+            vel.x = 0;
+        }
+        rb2d.velocity = vel;
+
+        var pos = transform.position;
+        if (pos.x > boundX)
+        {
+            pos.x = boundX;
+        }
+        else if (pos.x < -boundX)
+        {
+            pos.x = -boundX;
+        }
+        transform.position = pos;
+
+        if (Input.GetKeyDown(Shoot))
+        {
+            ShootLazer();
+        }
+    }
+
+    private void ShootLazer()
+    {
+        Instantiate(missel, transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("lazer"))
+        {
+            hit.Invoke();
+        }
+
+        if (collision.gameObject.CompareTag("invader"))
+        {
+            gameOver.Invoke();
+        }
     }
 }
