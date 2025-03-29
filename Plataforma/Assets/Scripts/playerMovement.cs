290c648033af3a1ae9583746ffb27579fd9ff3d6
JesCat16 <jmdjesus83@gmail.com>
2025-03-29 18:39:34 -0300

    add player

---
+++ Plataforma/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float Move;
+
+    public float speed;
+
+    public float jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+        }
+
+    }
+}
