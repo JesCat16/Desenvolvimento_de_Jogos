e3596838122638e891e1e0603c1da10f0d527e37
JesCat16 <jmdjesus83@gmail.com>
2025-03-31 15:01:46 -0300

    Animations

--- Plataforma/Assets/Scripts/playerMovement.cs
+++ Plataforma/Assets/Scripts/playerMovement.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-
     private float Move;
-
     public float speed;
-
     public float jump;
+    public Vector2 collisionGround;
+    public float seeGround;
+    public LayerMask ground;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -25,10 +27,38 @@ public class playerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && isInGround())
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
         }
 
+        if (Move != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        animator.SetBool("isJumping", !isInGround());
+
+    }
+
+    public bool isInGround()
+    {
+        if (Physics2D.BoxCast(transform.position, collisionGround, 0, -transform.up, seeGround, ground))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * seeGround, collisionGround);
     }
 }
